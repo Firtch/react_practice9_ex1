5bfca2aa15a55a11503d2d0be49242f874a2cee6
Firtch <firtch@gmail.com>
2021-06-13 16:16:27 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useContext, useState } from "react";
+
+export const ThemeContext = React.createContext("BLACK");
+// export const value = useContext(ThemeContext);
 
 function App() {
+  const [theme, setTheme] = useState("BLACK");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeContext.Provider value={theme}>
+        <button onClick={() => setTheme(theme === "BLACK" ? "RED" : "BLACK")}>
+          Смена Темы
+        </button>
+        <LabelBox value="Class component" />
+        <LabelBox2 value="Hook component" />
+      </ThemeContext.Provider>
     </div>
   );
+}
+
+export class LabelBox extends React.Component {
+  static contextType = ThemeContext;
+  render() {
+    console.log(this.context);
+    return <p className={this.context}>{this.props.value}</p>;
+  }
+}
+
+function LabelBox2(props) {
+  const value = useContext(ThemeContext);
+  return <p className={value}>{props.value}</p>;
 }
 
 export default App;
